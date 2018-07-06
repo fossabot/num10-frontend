d33b24a629d05f6e78ccd323219a6104510c1855
phongduong <s2pkmol@gmail.com>
2018-07-06 20:08:18 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -18,11 +18,10 @@ const PrivateRoute = () => {
 const view = (state, actions) => (
   <div id="container">
     <Switch>
-      <Route path="/404" render={Page404} />
+      <Route path="/" render={Dashboard} />
       <Route path="/login" render={Login} />
       <PrivateRoute />
-      <Route path="/" render={Dashboard} />
-      <Redirect to="/404" />
+      <Redirect to="/" />
     </Switch>
   </div>
 );
